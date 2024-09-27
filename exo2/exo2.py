553675b0c0c528b0a93e5ef54cb5b2cff9ef4b56
Jesus-flower <lethyciasarah.chahotanon@centrale-casablanca.ma>
2024-09-27 08:04:07 +0100

    exercice 2

--- exo2/exo2.py
+++ exo2/exo2.py
@@ -7,6 +7,47 @@ Examples:
     solution('abc', 'bc') # returns true
     solution('abc', 'd') # returns false
 """
+def solution(a, b):
+    return a.endswith(b)
+
+
+import unittest
+
+
+class TestEndsWith(unittest.TestCase):
+
+    def test_fixed_tests_true(self):
+        fixed_tests_True = (
+            ("samurai", "ai"),
+            ("ninja", "ja"),
+            ("sensei", "i"),
+            ("abc", "abc"),
+            ("abcabc", "bc"),
+            ("fails", "ails")
+        )
+
+        for s, ending in fixed_tests_True:
+            with self.subTest(s=s, ending=ending):
+                self.assertTrue(solution(s, ending))
+
+    def test_fixed_tests_false(self):
+        fixed_tests_False = (
+            ("sumo", "omo"),
+            ("samurai", "ra"),
+            ("abc", "abcd"),
+            ("ails", "fails"),
+            ("this", "fails"),
+            ("spam", "eggs")
+        )
+
+        for s, ending in fixed_tests_False:
+            with self.subTest(s=s, ending=ending):
+                self.assertFalse(solution(s, ending))
+
+
+# Si vous exécutez ce fichier directement, cela exécutera les tests
+if __name__ == '__main__':
+    unittest.main()
 
 """
 Create unit test using those cases:
